e0145a3b6fa510144f0de713d8747812b0bdb715
SagarikaR <32089555+SagarikaR@users.noreply.github.com>
2018-07-03 10:17:33 +0530

    new helloooo

--- samplegrey.py
+++ samplegrey.py
@@ -6,4 +6,4 @@ cv2.imshow('color_image',image)
 cv2.imshow('gray_image',gray_image) 
 cv2.waitKey(0)                 # Waits forever for user to press any key
 cv2.destroyAllWindows()        # Closes displayed windows
- 
+ #######################################
